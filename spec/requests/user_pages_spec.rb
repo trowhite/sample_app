4455d68ad6b1b83b30001d04ef571249160d590c
Matthew Q White <matthew.qualy.white@gmail.com>
2012-08-29 12:16:43 -0700

    adding user to db

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,4 +10,14 @@ describe "UserPages" do
     it { should have_selector('h1',		text: 'Sign up' ) }
     it { should have_selector('title',	text: full_title('Sign up')) }
   end
+  
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
 end
